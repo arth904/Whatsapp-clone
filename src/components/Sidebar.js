5bf08260e6a4c2458e8877798e799e22954a081d
Udbhava <udbhava15@gmail.com>
2021-04-15 17:30:38 +0530

    sidebar edit

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,34 +1,36 @@
-import React from 'react'
-import './Sidebar.css'
-import { Avatar } from "@material-ui/core";
+import React from "react";
+import "./Sidebar.css";
+import { Avatar, IconButton } from "@material-ui/core";
 import DonutLargeIcon from "@material-ui/icons/DonutLarge";
 import ChatIcon from "@material-ui/icons/Chat";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
-import SearchIcon from "@material-ui/icons/Search";
+import { SearchOutlined } from "@material-ui/icons";
 
 function Sidebar() {
-    return (
-        <div className='sidebar'>
-            <div className="sidebar__header">
-                <div className="header__user">
-                    <div className="user__img">
-                        <Avatar className='icon' src="https://pbs.twimg.com/profile_images/960126263447797761/_fAqgbu7.jpg" />
-                    </div>
-                    <div className="user__props">
-                        <DonutLargeIcon className='icon' />
-                        <ChatIcon className='icon' />
-                        <MoreVertIcon className='icon' />
-                    </div>
-                </div>
-            </div>
-            <div className="sidebar__input">
-                <div className="input">
-                    <SearchIcon className='icon'/>
-                    <input type="text" placeholder='search or start a new chat'/>
-                </div>
-            </div>
+  return (
+    <div className="sidebar">
+      <div className="sidebar__header">
+        <Avatar src="https://pbs.twimg.com/profile_images/960126263447797761/_fAqgbu7.jpg" />
+        <div className="Sidebar__headerRight">
+          <IconButton>
+            <DonutLargeIcon />
+          </IconButton>
+          <IconButton>
+            <ChatIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
         </div>
-    )
+      </div>
+      <div className="sidebar__search">
+        <div className="sidebar__searchContainer">
+          <SearchOutlined />
+          <input type="text" placeholder="Search or start a new chat" />
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Sidebar
+export default Sidebar;
